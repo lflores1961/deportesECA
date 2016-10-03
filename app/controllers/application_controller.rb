4d25f9a403eed8bca06b7f3c901a1f427cfed2db
Fernando Flores <fernando.flores1961@gmail.com>
2016-10-03 18:38:07 -0500

    Add provisional landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Escuela de la Ciudad de Aguascalientes"
+    render html: "         work in progress "
+  end
 end
