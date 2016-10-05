89ae97b51aefbbdb2682cbc3de113a4de8ba3627
Fernando Flores <fernando.flores1961@gmail.com>
2016-10-05 15:14:14 -0500

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  class Programmer
+  end
+
+  class Fernando_Flores < Programmer
+         attr_reader :email, :phone, :web
+    def initialize
+      @email = %[ fernando.flores1961@gmail.com ]
+      @phone = { :mobile  => '(449) 264 6604',
+                 :home    => '(449) 915 9633' }
+      @web   = %[ http://luisflores.wordpress.com ]
+    end
+  end
+
+  $luis = Fernando_Flores.new
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Deportes ECA"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
